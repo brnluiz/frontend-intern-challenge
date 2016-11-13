7a5e67d48c89d697d5765abb3e3f3aaa65d159f5
Bruno Luiz <contact@brunoluiz.net>
2016-11-13 02:01:14 -0200

    Added some other stuff to the gulpfile... analyzing the use of ReactJS (deadline)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,34 +3,45 @@ import sass       from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import concat     from 'gulp-concat';
 import gutil      from 'gulp-util';
+import babel      from 'gulp-babel'
 
 const public_path    = './public';
 const scss_src_path = './src/styles/**/*.scss';
 const js_src_path   = './src/js/**/*.js';
+const jsx_src_path  = './src/js/**/*.jsx';
 
 const assets_path = public_path + '/assets/';
 
 // SASS Rendering
-gulp.task('scss-render', function() {
-    gulp.src(scss_src_path)
-        .pipe(sourcemaps.init()) // Add the map to modified source.
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write()) // Add the map to modified source.
-        .pipe(gulp.dest(assets_path + '/styles'));
-});
+gulp.task('scss-render', () => gulp
+  .src(scss_src_path)
+  .pipe(sourcemaps.init()) // Add the map to modified source.
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write()) // Add the map to modified source.
+  .pipe(gulp.dest(assets_path + '/styles'))
+);
 
 // JS minify and concat
-gulp.task('js-build', function() {
-  return gulp.src(js_src_path)
-    .pipe(sourcemaps.init())
-    .pipe(concat('bundle.js'))
-    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) //only uglify if gulp is ran with '--type production'
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(assets_path + '/js'));
-});
+gulp.task('js-build', () => gulp
+  .src(jsx_src_path)
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+    "presets": ["react"]
+  }))
+  .pipe(concat('app.js'))
+  .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) //only uglify if gulp is ran with '--type production'
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(assets_path + '/js'))
+);
+
+gulp.task('html-copy', () => gulp
+  .src('./src/index.html')
+  .pipe(gulp.dest('./public'))
+);
 
 // Watch task
-gulp.task('default',function() {
-    gulp.watch(scss_src_path,['scss-render']);
-    gulp.watch(js_src_path,['js-build']);
+gulp.task('default', () => {
+  gulp.watch(scss_src_path,['scss-render']);
+  gulp.watch(jsx_src_path,['js-build']);
+  gulp.watch('./src/index.html',['html-copy']);
 });
